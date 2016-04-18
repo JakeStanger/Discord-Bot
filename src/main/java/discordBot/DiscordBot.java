3c6a52b07712c6575be5192ee11190d12695d253
Roboguy99 <Roboguy99@gmail.com>
2016-04-18 19:17:36 +0100

    Created main class

--- src/main/java/discordBot/DiscordBot.java
+++ src/main/java/discordBot/DiscordBot.java
@@ -1,9 +1,14 @@
 package discordBot;
 
+import bot.Bot;
+import window.Window;
+
 public class DiscordBot
-{
+{	
 	public static void main(String[] args)
 	{
-		
+		//new Play().init();
+		new Window();
+		new Bot();
 	}
 }
