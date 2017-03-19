f6e8bafd0dececcf5f503b264902c9e78f6e99d5
Jake Stanger <jakestanger@gmail.com>
2017-03-19 11:34:46 +0000

    Split audio parts of bot into new bot

--- src/main/java/com/jakestanger/discordBot/Bot.java
+++ src/main/java/com/jakestanger/discordBot/Bot.java
@@ -4,27 +4,14 @@ import com.github.fedy2.weather.YahooWeatherService;
 import com.github.fedy2.weather.data.*;
 import com.github.fedy2.weather.data.unit.DegreeUnit;
 import com.github.fedy2.weather.data.unit.Time;
-import com.jakestanger.discordBot.audio.GuildMusicManager;
 import com.jakestanger.discordBot.util.Lyrics;
 import com.jakestanger.discordBot.util.Phrases;
-import com.jakestanger.discordBot.util.ReadWrite;
 import com.jakestanger.discordBot.wrapper.Song;
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-import net.dv8tion.jda.core.managers.AudioManager;
 import net.dv8tion.jda.core.utils.SimpleLog;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -37,7 +24,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Bot extends ListenerAdapter
 {
@@ -45,22 +35,11 @@ public class Bot extends ListenerAdapter
 	
 	private SimpleLog logger;
 	
-	private HashMap<String, String> sounds;
-	
-	private final AudioPlayerManager playerManager;
-	private final Map<Long, GuildMusicManager> musicManagers;
-	
 	public Bot()
 	{
 		instance = this;
 		logger = SimpleLog.getLog("Bot");
 		logger.info("Creating new instance of Bot");
-		//sounds = ReadWrite.readSounds();
-		
-		musicManagers = new HashMap<>();
-		playerManager = new DefaultAudioPlayerManager();
-		AudioSourceManagers.registerRemoteSources(playerManager);
-		AudioSourceManagers.registerLocalSource(playerManager);
 	}
 	
 	public static Bot getInstance()
@@ -106,15 +85,6 @@ public class Bot extends ListenerAdapter
 		return success;
     }
 	
-	private synchronized GuildMusicManager getGuildAudioPlayer(Guild guild)
-	{
-		long guildId = Long.parseLong(guild.getId());
-		GuildMusicManager musicManager = musicManagers.computeIfAbsent(guildId, k -> new GuildMusicManager(playerManager));
-		
-		guild.getAudioManager().setSendingHandler(musicManager.getSendHandler());
-		
-		return musicManager;
-	}
 	 
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event)
@@ -130,32 +100,6 @@ public class Bot extends ListenerAdapter
 			String[] command = message.substring(("!").length()).split(" ");
 			switch(command[0].toLowerCase())
 			{
-				case "play":
-					if(command.length > 1)
-						if (this.sounds.containsKey(command[1])) loadAndPlay(this.sounds.get(command[1]), channel);
-						else loadAndPlay(command[1], channel);
-					else this.helpPlay(channel);
-					break;
-				case "pause":
-					togglePause(channel);
-					break;
-				case "skip":
-					skipTrack(channel);
-					break;
-				case "empty":
-					emptyQueue(channel);
-					break;
-				case "add":
-					if(command.length > 2) addSound(channel, command[1], command[2]);
-					else this.message(Phrases.UnknownCommand.getRandom(), channel);
-					break;
-				case "join":
-					if(command.length > 1) joinChannel(command[1], event);
-					else this.message(Phrases.NoChannel.getRandom(), channel);
-					break;
-				case "leave":
-					leaveChannel(event);
-					break;
 				case "lyrics":
 					if(command.length > 2) fetchLyrics(command[1], command[2], channel);
 					else this.message(Phrases.UnknownCommand.getRandom(), channel);
@@ -166,9 +110,6 @@ public class Bot extends ListenerAdapter
 					break;
 				case "weather":
 					weather(channel);
-					break;
-				case "reload":
-					reload(event);
 					break;
 				case "help":
 					help(channel);
@@ -181,151 +122,6 @@ public class Bot extends ListenerAdapter
 	}
 	
 	/**
-	 * Load and play the given audio stream
-	 * @param trackUrl The path to audio stream
-	 * @param channel The text channel
-	 */
-	private void loadAndPlay(String trackUrl, TextChannel channel)
-	{
-		GuildMusicManager musicManager = getGuildAudioPlayer(channel.getGuild());
-		
-		playerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
-			@Override
-			public void trackLoaded(AudioTrack track)
-			{
-				message("Adding *" + track.getInfo().title + "* to the queue", channel);
-				play(channel.getGuild(), musicManager, track);
-			}
-			
-			@Override
-			public void playlistLoaded(AudioPlaylist playlist)
-			{
-				AudioTrack firstTrack = playlist.getSelectedTrack();
-				
-				if(firstTrack == null) firstTrack = playlist.getTracks().get(0);
-				
-				message("Adding to queue " + firstTrack.getInfo().title + " (first track of playlist " + playlist.getName() + ")", channel);
-				
-				play(channel.getGuild(), musicManager, firstTrack);
-			}
-			
-			@Override
-			public void noMatches()
-			{
-				message("*" + trackUrl + "* is not a valid sound source", channel);
-			}
-			
-			@Override
-			public void loadFailed(FriendlyException exception)
-			{
-				message("An error occurred: *" + exception.getMessage() + "*", channel);
-				exception.printStackTrace();
-			}
-		});
-	}
-	
-	/**
-	 * Play the given track
-	 * @param guild The server
-	 * @param musicManager The music manager
-	 * @param track The track to play
-	 */
-	private void play(Guild guild, GuildMusicManager musicManager, AudioTrack track)
-	{
-		connectToFirstVoiceChannel(guild.getAudioManager());
-		musicManager.scheduler.queue(track);
-	}
-	
-	/**
-	 * Skip to the next track in the queue
-	 * @param channel The text channel
-	 */
-	private void skipTrack(TextChannel channel)
-	{
-		GuildMusicManager musicManager = getGuildAudioPlayer(channel.getGuild());
-		musicManager.scheduler.nextTrack();
-		
-		message("Skipped to next track.", channel);
-	}
-	
-	/**
-	 * Connect to the primary voice channel
-	 * @param audioManager The audio manager
-	 */
-	private static void connectToFirstVoiceChannel(AudioManager audioManager)
-	{
-		if(!audioManager.isConnected() && !audioManager.isAttemptingToConnect())
-			audioManager.openAudioConnection(audioManager.getGuild().getVoiceChannels().get(0));
-	}
-
-	
-	/**
-	 * Toggles whether the player is paused or not
-	 * @param channel The text channel the command was sent from
-	 */
-	private void togglePause(TextChannel channel)
-	{
-		GuildMusicManager musicManager = getGuildAudioPlayer(channel.getGuild());
-		musicManager.player.setPaused(!musicManager.player.isPaused());
-	}
-	
-	/**
-	 * Remove all the tracks from the queue.
-	 * Method is a bit unclean.
-	 * @param channel The text channel.
-	 */
-	private void emptyQueue(TextChannel channel)
-	{
-		GuildMusicManager musicManager = getGuildAudioPlayer(channel.getGuild());
-		while(musicManager.player.getPlayingTrack() != null) musicManager.scheduler.nextTrack();
-	}
-	
-	/**
-	 * Adds a sound to the database for quick-playing
-	 * @param channel The text channel
-	 * @param name The sound friendly name
-	 * @param url The sound URL
-	 */
-	private void addSound(TextChannel channel, String name, String url)
-	{
-		sounds.put(name, url);
-		ReadWrite.addSound(name, url);
-		message("Added sound *" + name + "* with url **" + url + "** to database.", channel);
-	}
-		
-	/**
-	 * Joins the given channel
-	 * @param channelName The name of the channel to join.
-	 * @param event The message received event.
-	 */
-	private void joinChannel(String channelName, MessageReceivedEvent event)
-	{
-		if(event.getMember().hasPermission(Permission.VOICE_CONNECT))
-		{
-			if(event.getGuild().getAudioManager() != null) event.getGuild().getAudioManager().closeAudioConnection(); //Close existing voice connection
-			//Scans through the VoiceChannels in this Guild, looking for one with a case-insensitive matching name.
-	        VoiceChannel channel = event.getGuild().getVoiceChannels().stream().filter(vChan -> vChan.getName().equalsIgnoreCase(channelName)).findFirst().orElse(null);
-	        if (channel == null)
-	        {
-	            this.message(Phrases.UnknownChannel.getRandom(), event.getTextChannel());
-	            return;
-	        }
-	        event.getGuild().getAudioManager().openAudioConnection(channel);
-		}
-		else this.message(Phrases.BadPermission.getRandom(), event.getTextChannel());
-	}
-	
-	/**
-	 * Leaves the current channel
-	 * @param event The message received event.
-	 */
-	private void leaveChannel(MessageReceivedEvent event)
-	{
-		if(event.getMember().hasPermission(Permission.VOICE_CONNECT)) event.getGuild().getAudioManager().closeAudioConnection();
-		else this.message(Phrases.BadPermission.getRandom(), event.getTextChannel());
-	}
-	
-	/**
 	 * Shows a list of commands and their purpose.
 	 * A little hard-coded, but there you go.
 	 * @param channel The message received event.
@@ -333,41 +129,11 @@ public class Bot extends ListenerAdapter
 	private void help(TextChannel channel)
 	{
 		this.message("***Commands:***\n"
-				+ "**play [sound]** - Shows a list of sounds or [plays the given sound]\n"
-				+ "**pause** - Toggle-pauses the audio player\n"
-				+ "**skip** - Skips the current audio track\n"
-				+ "**empty** - Empties the song queue\n"
-				+ "**add <sound> <url>** - Add a sound to the local database\n"
-				+ "**join <channel>** - Joins the given voice channel\n"
-				+ "**leave** - Leaves the current voice channel\n"
 				+ "**weather** - Gives a *very* useful weather report\n"
 				+ "**lyrics <band> <song>** - Shows the lyrics for the given song\n"
-				+ "**help** - Shows a list of commands\n", channel);
-	}
-	
-	/**
-	 * Shows a list of sounds
-	 * @param channel The message received event.
-	 */
-	private void helpPlay(TextChannel channel)
-	{
-		StringBuilder message = new StringBuilder();
-		List<String> names = new ArrayList<>(sounds.keySet());
-		names.sort(Comparator.naturalOrder());
-		
-		for(String name : names) message.append(name).append("\t\t**||**\t\t");
-		
-		message(message.toString(), channel);
-	}
-	
-	private void reload(MessageReceivedEvent event)
-	{
-		if(event.getMember().hasPermission(Permission.MANAGE_SERVER))
-		{
-			this.sounds = ReadWrite.readSounds();
-			this.message("Sounds reloaded", event.getTextChannel());
-		}
-		else this.message(Phrases.BadPermission.getRandom(), event.getTextChannel());
+				+ "**lyricsgame <band>** - Multiple choice lyrics quiz\n"
+				+ "**help** - Shows a list of commands\n"
+				+ "**@Grandad_Botbags** <message> - Chats to Botbags", channel);
 	}
 	
 	private void fetchLyrics(String band, String song, TextChannel channel)
--- src/main/java/com/jakestanger/discordBot/util/ReadWrite.java
+++ src/main/java/com/jakestanger/discordBot/util/ReadWrite.java
@@ -1,68 +1,17 @@
 package com.jakestanger.discordBot.util;
 
-import com.google.gson.*;
-import com.jakestanger.discordBot.wrapper.Song;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.dv8tion.jda.core.utils.SimpleLog;
 import net.dv8tion.jda.core.utils.SimpleLog.Level;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ReadWrite
 {
 	private static final SimpleLog logger = SimpleLog.getLog("ReadWrite");
-	
-	public static HashMap<String, String> readSounds()
-	{
-		HashMap<String, String> sounds = new HashMap<>();
-		
-		JsonParser parser = new JsonParser();
-		try
-		{
-			JsonObject object = parser.parse(new FileReader("/home/pi/Grandad_Botbags/sounds.json")).getAsJsonObject();
-			//JsonObject object = parser.parse(new FileReader("sounds.json")).getAsJsonObject();
-			
-			for(Map.Entry<String, JsonElement> sound : object.entrySet()) sounds.put(sound.getKey(), sound.getValue().getAsString());
-		}
-		catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return sounds;
-	}
-	
-	public static void addSound(String newName, String url)
-	{
-		HashMap<String, String> sounds = readSounds();
-		sounds.put(newName, url);
-		
-		JsonObject object = new JsonObject();
-		for(String name : sounds.keySet()) object.addProperty(name, sounds.get(name));
-		
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String json = gson.toJson(object);
-		
-		try
-		{
-			FileWriter writer = new FileWriter("/home/pi/Grandad_Botbags/sounds.json");
-			//FileWriter writer = new FileWriter("sounds.json");
-			writer.write(json);
-			writer.close();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	public static void writeSounds()
-	{
-		
-	}
 	
 	public static void readSettings()
 	{
@@ -132,24 +81,5 @@ public class ReadWrite
 			e.printStackTrace();
 		}
 		return token;
-	}
-	
-	public static List<Song> readLyrics(String artist)
-	{
-		List<Song> songs = new ArrayList<>();
-		
-		JsonParser parser = new JsonParser();
-		try
-		{
-			//JsonObject object = parser.parse(new FileReader("artists/" + artist + ".json")).getAsJsonObject();
-			JsonObject object = parser.parse(new FileReader("/home/pi/Grandad_Botbags/artists/" + artist + ".json")).getAsJsonObject();
-			System.out.println(object);
-		}
-		catch (FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return songs;
 	}
 }
